e30a68ac85ba1e329871749f7a0c91706ee070b5
mosuswalks <mosuswalks@gmail.com>
2019-02-25 16:41:44 -0500

    Changes to index.js for serving static files

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const request = require("request");
+const path = require("path");
 
 quoteUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en"
 const PORT = process.env.PORT || 3030;
 
+app.use(express.static(path.join(__dirname, 'frontend/build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+})
 
 // WARNING: YOU SHOULD CONFIGURE CORS TO NOT ALLOW ACCESS UNLESS SPECIFIED.
 app.get("/quote", cors(),(req, res) =>{
